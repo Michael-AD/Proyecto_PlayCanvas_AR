cc2bd7310612e1741a69d53cb9efc234e93bffb3
Michael-AD <74881228+Michael-AD@users.noreply.github.com>
2024-09-24 09:41:53 -0600

    Initial Commit AR Project
    Comitt inicial de AR Project con Play Canvas

---
+++ WebXR AR Spiderman/__settings__.js
@@ -0,0 +1,22 @@
+window.ASSET_PREFIX = "";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "2076112.json";
+window.CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'deviceTypes': [`webgl2`, `webgl1`],
+    'powerPreference': "high-performance"
+};
+window.SCRIPTS = [ 196818284, 196818287, 196818315, 196818295, 196818296, 196818297, 196818298, 196818299, 196818301, 196818302, 196818303, 196818304, 196818306, 196818307, 196968426 ];
+window.CONFIG_FILENAME = "config.json";
+window.INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+window.PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/196818312/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/196818313/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/196818311/1/ammo.js', 'preload' : true},
+];
